77149fab1fc6d4afb532f9f66bf7fc8121a8a33f
Aswin-Shaji <aswin2000.ak@gmail.com>
2023-01-23 17:22:45 +0530

    Add 6-allLongestString solution

--- 6-allLongestStrings/allLongestStrings.ts
+++ 6-allLongestStrings/allLongestStrings.ts
@@ -1,5 +1,18 @@
 export function allLongestStrings(inputArray: string[]): string[] {
+  let longestString = 0;
 
+  inputArray.forEach((element) => {
+    longestString = element.length > longestString ? element.length : longestString;
+  });
+
+  const newArray: string[] = [];
+  inputArray.forEach((element) => {
+    if (element.length === longestString) {
+      newArray.push(element);
+    }
+  });
+
+  return newArray;
 }
 
 // console.log(allLongestStrings(["aba", "aa", "ad", "vcd", "aba"]));
