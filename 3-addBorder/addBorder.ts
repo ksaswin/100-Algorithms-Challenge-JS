df76fbe61613c722de12d667301bd6eb4c46691e
Aswin-Shaji <aswin2000.ak@gmail.com>
2023-01-23 16:24:04 +0530

    Add 3-addBorder solution

--- 3-addBorder/addBorder.ts
+++ 3-addBorder/addBorder.ts
@@ -1,5 +1,14 @@
 export function addBorder(picture: string[]): string[] {
+  const wall = '*'.repeat(picture[0].length + 2);
 
+  for (let i=0; i<picture.length; i++) {
+    picture[i] = '*'.concat(picture[i], '*');
+  }
+
+  picture.push(wall);
+  picture.unshift(wall);
+
+  return picture;
 }
 
 // console.log(addBorder(["abc", "ded"]));
