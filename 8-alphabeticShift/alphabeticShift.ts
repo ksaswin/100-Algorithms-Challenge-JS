b33141712803aa2874b2f335fd8e26102e0a5cf3
Aswin-Shaji <aswin2000.ak@gmail.com>
2023-01-23 18:12:44 +0530

    Add 8-alphabeticShift solution

--- 8-alphabeticShift/alphabeticShift.ts
+++ 8-alphabeticShift/alphabeticShift.ts
@@ -1,5 +1,13 @@
 export function alphabeticShift(inputString: string): string {
+  let newString = inputString.split('').map((element) => {
+    if (element === 'z') {
+      return 'a';
+    }
 
+    return String.fromCharCode(element.charCodeAt(0) + 1);
+  });
+
+  return newString.join('');
 }
 
-console.log(alphabeticShift('crazy'));
+// console.log(alphabeticShift('crazy'));
