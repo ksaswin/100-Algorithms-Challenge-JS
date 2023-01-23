91d2e4a3ab094f92619ec4dea7e3b873c9f845cb
Aswin-Shaji <aswin2000.ak@gmail.com>
2023-01-23 18:30:27 +0530

    Add 9-alphabetSubSequence solution

--- 9-alphabetSubSequence/alphabetSubSequence.ts
+++ 9-alphabetSubSequence/alphabetSubSequence.ts
@@ -1,5 +1,15 @@
 export function alphabetSubsequence(s: string): boolean {
+  if (s.length !== (new Set(s)).size) {
+    return false;
+  }
 
+  for (let i=0; i<s.length-1; i++) {
+    if (s.charCodeAt(i) > s.charCodeAt(i+1)) {
+      return false;
+    }
+  }
+
+  return true;
 }
 
 // console.log(alphabetSubsequence('zab'))
