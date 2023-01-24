cacb39d0f33e2426efa75f6d8b9354ec85fbbe99
Aswin-Shaji <aswin2000.ak@gmail.com>
2023-01-24 12:24:25 +0530

    Add 10-alternatingSums solution

--- 10-alternatingSums/alternatingSums.ts
+++ 10-alternatingSums/alternatingSums.ts
@@ -1,4 +1,15 @@
 export function alternatingSums(a: number[]): number[] {
+  const sum = [0, 0];
+  a.forEach((weight, index) => {
+    if (index%2 === 0) {
+      sum[0] += weight;
+    }
+    else {
+      sum[1] += weight;
+    }
+  });
+
+  return sum;
 }
 
 // console.log(alternatingSums([50, 60, 60, 45, 70]))
