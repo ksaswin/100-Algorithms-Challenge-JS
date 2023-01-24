7b3455fb37c0b12ab8d0e12808347c8207824372
Aswin-Shaji <aswin2000.ak@gmail.com>
2023-01-24 13:45:51 +0530

    Add 13-arrayChange solution

--- 13-arrayChange/arrayChange.ts
+++ 13-arrayChange/arrayChange.ts
@@ -1,4 +1,16 @@
 export function arrayChange(inputArray: number[]): number {
+  let moves = 0;
+  let change = 0;
+
+  for (let i=1; i<inputArray.length; i++) {
+    if (inputArray[i] <= inputArray[i-1]) {
+      change = inputArray[i-1] - inputArray[i] + 1;
+      inputArray[i] += change;
+      moves += change;
+    }
+  }
+
+  return moves;
 }
 
 // console.log(arrayChange([1, 1, 1]));
