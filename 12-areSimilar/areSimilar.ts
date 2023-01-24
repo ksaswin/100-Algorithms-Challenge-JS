32af286d07595d40e6e7894016cec9af45b0e4d4
Aswin-Shaji <aswin2000.ak@gmail.com>
2023-01-24 13:16:55 +0530

    Add 12-areSimilar solution

--- 12-areSimilar/areSimilar.ts
+++ 12-areSimilar/areSimilar.ts
@@ -1,4 +1,27 @@
 export function areSimilar(a: number[], b: number[]): boolean {
+  if (a.toString() === b.toString()) {
+    return true;
+  }
+
+  const arrayC: Array<number> = [];
+  const arrayD: Array<number> = [];
+
+  for (let i=0; i<a.length; i++) {
+    if (a[i] !== b[i]) {
+      arrayC.push(a[i]);
+      arrayD.push(b[i]);
+    }
+  }
+
+  if (arrayC.length > 2) {
+    return false;
+  }
+
+  if (arrayC.toString() === arrayD.reverse().toString()) {
+    return true;
+  }
+
+  return false;
 }
 
 // console.log(areSimilar([1, 2, 3], [1, 2, 3]));
