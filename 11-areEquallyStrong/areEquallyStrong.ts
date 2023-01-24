6bc097ba79c0c1fd2b682e5188fefc2084879654
Aswin-Shaji <aswin2000.ak@gmail.com>
2023-01-24 12:43:44 +0530

    Add 11-areEquallyStrong solution

--- 11-areEquallyStrong/areEquallyStrong.ts
+++ 11-areEquallyStrong/areEquallyStrong.ts
@@ -1,5 +1,8 @@
 export function areEquallyStrong(yourLeft: number, yourRight: number, friendsLeft: number, friendsRight: number): boolean {
+  const [yourStrength, yourWeakness] = yourRight > yourLeft ? [yourRight, yourLeft] : [yourLeft, yourRight];
+  const [friendStrength, friendWeakness] = friendsRight > friendsLeft ? [friendsRight, friendsLeft] : [friendsLeft, friendsRight];
 
+  return (friendStrength === yourStrength) && (friendWeakness === yourWeakness);
 }
 
 // console.log(areEquallyStrong(10, 15, 15, 10))
