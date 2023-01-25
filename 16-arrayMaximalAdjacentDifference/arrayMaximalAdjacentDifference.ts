be76a916899784b0128cc5b09772cedf4fe3f522
Aswin-Shaji <aswin2000.ak@gmail.com>
2023-01-25 14:40:39 +0530

    Add 16-arrayMaximalAdjacentDifference solution

--- 16-arrayMaximalAdjacentDifference/arrayMaximalAdjacentDifference.ts
+++ 16-arrayMaximalAdjacentDifference/arrayMaximalAdjacentDifference.ts
@@ -1,5 +1,16 @@
 export function arrayMaximalAdjacentDifference(inputArray: number[]): number {
+  let difference = Math.abs(inputArray[0] - inputArray[1]);
+  let maximalDifference: number = difference;
 
+  for (let i=1; i<inputArray.length-1; i++) {
+    difference = Math.abs(inputArray[i] - inputArray[i+1]);
+
+    if (difference > maximalDifference) {
+      maximalDifference = difference;
+    }
+  }
+
+  return maximalDifference;
 }
 
 // console.log(arrayMaximalAdjacentDifference([2, 4, 1, 0]));
