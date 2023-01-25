31803528d78bb8ce5bb31c39fec57d533b38b491
Aswin-Shaji <aswin2000.ak@gmail.com>
2023-01-25 15:22:56 +0530

    Add 18-arrayReplace solution

--- 18-arrayReplace/arrayReplace.ts
+++ 18-arrayReplace/arrayReplace.ts
@@ -1,4 +1,11 @@
 export function arrayReplace(inputArray: number[], elemToReplace: number, substitutionElem: number): number[] {
+  inputArray.forEach((element, index) => {
+    if (element === elemToReplace) {
+      inputArray[index] = substitutionElem;
+    }
+  });
+
+  return inputArray;
 }
 
 // console.log(arrayReplace([1, 2, 1], 1, 3));
