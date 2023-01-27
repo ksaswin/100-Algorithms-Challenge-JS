e2a59d772c36066f5559c4b75ad54788b0574abe
Aswin-Shaji <aswin2000.ak@gmail.com>
2023-01-27 17:55:58 +0530

    Update 20-bishopAndPawn unit tests

--- 20-bishopAndPawn/bishopAndPawn.spec.ts
+++ 20-bishopAndPawn/bishopAndPawn.spec.ts
@@ -12,4 +12,76 @@ describe(bishopAndPawn.name, () => {
         // assert
         expect(response).toBe(true);
     });
+
+    it('Test 2', () => {
+        // arrange
+        const position1 = 'e4';
+        const position2 = 'h1';
+
+        // act
+        const response = bishopAndPawn(position1, position2);
+
+        // assert
+        expect(response).toBe(true);
+    });
+
+    it('Test 3', () => {
+        // arrange
+        const position1 = 'e4';
+        const position2 = 'c2';
+
+        // act
+        const response = bishopAndPawn(position1, position2);
+
+        // assert
+        expect(response).toBe(true);
+    });
+
+    it('Test 4', () => {
+        // arrange
+        const position1 = 'e4';
+        const position2 = 'b7';
+
+        // act
+        const response = bishopAndPawn(position1, position2);
+
+        // assert
+        expect(response).toBe(true);
+    });
+
+    it('Test 5', () => {
+        // arrange
+        const position1 = 'e4';
+        const position2 = 'e1';
+
+        // act
+        const response = bishopAndPawn(position1, position2);
+
+        // assert
+        expect(response).toBe(false);
+    });
+
+    it('Test 6', () => {
+        // arrange
+        const position1 = 'e4';
+        const position2 = 'a4';
+
+        // act
+        const response = bishopAndPawn(position1, position2);
+
+        // assert
+        expect(response).toBe(false);
+    });
+
+    it('Test 7', () => {
+        // arrange
+        const position1 = 'e4';
+        const position2 = 'h6';
+
+        // act
+        const response = bishopAndPawn(position1, position2);
+
+        // assert
+        expect(response).toBe(false);
+    });
 });
