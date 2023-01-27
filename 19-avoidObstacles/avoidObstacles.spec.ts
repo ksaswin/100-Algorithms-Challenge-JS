d1a3f785d9db3025d1a9ba4d784e0c3102669697
Aswin-Shaji <aswin2000.ak@gmail.com>
2023-01-27 16:04:56 +0530

    Add 19-avoidObstacles solution

--- 19-avoidObstacles/avoidObstacles.spec.ts
+++ 19-avoidObstacles/avoidObstacles.spec.ts
@@ -12,4 +12,16 @@ describe(avoidObstacles.name, () => {
         // assert
         expect(response).toBe(4);
     });
+
+    it('Test 2', () => {
+        // arrange
+        const data = [5, 3, 6, 7, 9, 10, 11, 12];
+
+
+        // act
+        const response = avoidObstacles(data);
+
+        // assert
+        expect(response).toBe(8);
+    });
 });
--- 19-avoidObstacles/avoidObstacles.ts
+++ 19-avoidObstacles/avoidObstacles.ts
@@ -1,4 +1,27 @@
+// This solution is from
+// https://www.geeksforgeeks.org/minimum-length-of-jumps-to-avoid-given-array-of-obstacles/
 export function avoidObstacles(inputArray: number[]): number {
+  const obstacleSet = new Set(inputArray);
+  const max = Math.max(...inputArray);
+
+  // checking for every possible length which
+  // yield us solution
+  for (let i = 1; i <= max; i++) {
+    let j: number;
+    for (j = i; j <= max; j = j + i) {
+      // if there is obstacle, break the loop.
+      if (obstacleSet.has(j)) {
+        break;
+      }
+    }
+
+    // If above loop did not break
+    if (j > max) {
+      return i;
+    }
+  }
+
+  return max + 1;
 }
 
 // console.log(avoidObstacles([5, 3, 6, 7, 9]));
