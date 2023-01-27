1e0b0fddf4724bd2ce47694afe00054170f7449b
Aswin-Shaji <aswin2000.ak@gmail.com>
2023-01-27 14:42:02 +0530

    Add 11-areEquallyStrong new solution

--- 11-areEquallyStrong/areEquallyStrong.ts
+++ 11-areEquallyStrong/areEquallyStrong.ts
@@ -1,9 +1,18 @@
 export function areEquallyStrong(yourLeft: number, yourRight: number, friendsLeft: number, friendsRight: number): boolean {
-  const [yourStrength, yourWeakness] = yourRight > yourLeft ? [yourRight, yourLeft] : [yourLeft, yourRight];
-  const [friendStrength, friendWeakness] = friendsRight > friendsLeft ? [friendsRight, friendsLeft] : [friendsLeft, friendsRight];
+  const [ yourSum, friendsSum ] = [ (yourLeft + yourRight), (friendsLeft + friendsRight) ];
+  const [ yourStrength, friendStrength ] = [ Math.max(yourLeft, yourRight), Math.max(friendsLeft, friendsRight) ];
+
+  const [yourWeakness, friendWeakness] = [ (yourSum - yourStrength), (friendsSum - friendStrength) ];
 
   return (friendStrength === yourStrength) && (friendWeakness === yourWeakness);
 }
+
+// export function areEquallyStrong(yourLeft: number, yourRight: number, friendsLeft: number, friendsRight: number): boolean {
+//   const [yourStrength, yourWeakness] = yourRight > yourLeft ? [yourRight, yourLeft] : [yourLeft, yourRight];
+//   const [friendStrength, friendWeakness] = friendsRight > friendsLeft ? [friendsRight, friendsLeft] : [friendsLeft, friendsRight];
+//
+//   return (friendStrength === yourStrength) && (friendWeakness === yourWeakness);
+// }
 
 // console.log(areEquallyStrong(10, 15, 15, 10))
 // console.log(areEquallyStrong(15, 10, 15, 10))
