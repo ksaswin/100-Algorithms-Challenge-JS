f8af107571ea8ca08ec16a9f113ad8db59406b9f
Aswin-Shaji <aswin2000.ak@gmail.com>
2023-01-27 17:56:11 +0530

    Add 20-bishopAndPawn solution

--- 20-bishopAndPawn/bishopAndPawn.ts
+++ 20-bishopAndPawn/bishopAndPawn.ts
@@ -1,5 +1,20 @@
 export function bishopAndPawn(bishop: string, pawn: string): boolean {
+  const columnMarkers = { a: 1, b: 2, c: 3, d: 4, e: 5, f: 6, g: 7, h: 8 };
 
+  const bishopPosition = [ columnMarkers[bishop[0]], parseInt(bishop[1]) ];
+  const pawnPosition = [ columnMarkers[pawn[0]], parseInt(pawn[1]) ];
+
+  if (bishopPosition[0] === pawnPosition[0] || bishopPosition[1] === pawnPosition[1]) {
+    return false;
+  }
+
+  const difference = [ (Math.abs(bishopPosition[0] - pawnPosition[0])), (Math.abs(bishopPosition[1] - pawnPosition[1])) ];
+
+  if (difference[0] === difference[1]) {
+    return true;
+  }
+
+  return false;
 }
 
 // console.log(bishopAndPawn('a1', 'c3'));
