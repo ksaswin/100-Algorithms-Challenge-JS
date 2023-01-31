00c88abf65f19e455ea2fd9dbb2ccd6b910ee886
Aswin-Shaji <aswin2000.ak@gmail.com>
2023-01-31 16:28:21 +0530

    Update 21-boxBlur unit test

--- 21-boxBlur/boxBlur.spec.ts
+++ 21-boxBlur/boxBlur.spec.ts
@@ -13,4 +13,18 @@ describe(boxBlur.name, () => {
         // assert
         expect(response).toEqual([[1]]);
     });
+
+    it('Test 2', () => {
+        // arrange
+        const data = [[7, 4, 0, 1],
+        [5, 6, 2, 2],
+        [6, 10, 7, 8],
+        [1, 4, 2, 0]];
+
+        // act
+        const response = boxBlur(data);
+
+        // assert
+        expect(response).toEqual([[5, 4], [4, 4]]);
+    });
 });
