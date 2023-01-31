3572f6586fcbdc4d3dfa0c0b8ebb16e7a4c4b2f5
Aswin-Shaji <aswin2000.ak@gmail.com>
2023-01-31 16:28:33 +0530

    Add 21-boxBlur solution

--- 21-boxBlur/boxBlur.ts
+++ 21-boxBlur/boxBlur.ts
@@ -1,4 +1,28 @@
 export function boxBlur(image: number[][]): number[][] {
+  let blur: Array<Array<number>> = [];
+  const length = image.length;
+  let sum = 0;
+  let line: Array<number> = [];
+
+  for (let i = 0; i < length - 2; i++) {
+    line = [];
+
+    for (let j = 0; j < length - 2; j++) {
+      sum = 0;
+
+      for (let a = i; a < i + 3; a++) {
+        for (let b = j; b < j + 3; b++) {
+          sum += image[a][b];
+        }
+      }
+
+      line.push(Math.floor(sum / 9));
+    }
+
+    blur.push(line);
+  }
+
+  return blur
 }
 
 // console.log(boxBlur([[1, 1, 1], 
