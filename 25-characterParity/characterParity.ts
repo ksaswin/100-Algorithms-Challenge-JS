9c1ef6e60cf95914e2cc64e58bea3ddc884491ef
Aswin-Shaji <aswin2000.ak@gmail.com>
2023-01-31 17:24:40 +0530

    Add 25-characterParity solution

--- 25-characterParity/characterParity.ts
+++ 25-characterParity/characterParity.ts
@@ -1,4 +1,11 @@
 export function characterParity(symbol: string): string {
+  const parsed = parseInt(symbol);
+
+  if (isNaN(parsed)) {
+    return 'not a digit'
+  }
+
+  return parsed % 2 === 0 ? 'even' : 'odd';
 }
 
 // console.log(characterParity('5'))
