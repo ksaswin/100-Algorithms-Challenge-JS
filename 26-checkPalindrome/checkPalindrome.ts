e0929ff8e45be3e2868d51103b5335dbe8119cf0
Aswin-Shaji <aswin2000.ak@gmail.com>
2023-01-31 17:54:01 +0530

    Add 26-checkPalindrome new solution

--- 26-checkPalindrome/checkPalindrome.ts
+++ 26-checkPalindrome/checkPalindrome.ts
@@ -1,6 +1,19 @@
 export function checkPalindrome(inputString: string): boolean {
-  return inputString === inputString.split('').reverse().join('');
+  const length = inputString.length;
+
+  for (let i = 0; i < length / 2; i++) {
+    if (inputString[i] !== inputString[length - i - 1]) {
+      return false;
+    }
+  }
+
+  return true;
 }
+
+// Solving the problem with methods available in JS
+// export function checkPalindrome(inputString: string): boolean {
+//   return inputString === inputString.split('').reverse().join('');
+// }
 
 // console.log(checkPalindrome('aabaa'));
 // console.log(checkPalindrome('abac'));
