e2305d47ec9c73121086748d54a37f6c14e4153a
Aswin-Shaji <aswin2000.ak@gmail.com>
2023-01-31 16:55:15 +0530

    Add 23-caseInsensitivePalindrome solution

--- 23-caseInsensitivePalimdrome/caseInsensitivePalindrome.ts
+++ 23-caseInsensitivePalimdrome/caseInsensitivePalindrome.ts
@@ -1,4 +1,8 @@
 export function isCaseInsensitivePalindrome(inputString: string): boolean {
+  inputString = inputString.toLowerCase();
+  const reversedString = inputString.split('').reverse().join('');
+
+  return reversedString === inputString;
 }
 
 // console.log(isCaseInsensitivePalindrome('AaBaa'));
