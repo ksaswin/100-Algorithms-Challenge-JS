57182898744b8dde2685b90b3a8fae272ad56ead
Aswin-Shaji <aswin2000.ak@gmail.com>
2023-01-31 17:11:24 +0530

    Add 24-centuryFromYear solution

--- 24-centuryFromYear/centuryFromYear.ts
+++ 24-centuryFromYear/centuryFromYear.ts
@@ -1,4 +1,11 @@
 export function centuryFromYear(year: number): number {
+  const century = Math.floor(year / 100);
+
+  if (year % 100 === 0) {
+    return century;
+  }
+
+  return century + 1;
 }
 
 // console.log(centuryFromYear(1905));
