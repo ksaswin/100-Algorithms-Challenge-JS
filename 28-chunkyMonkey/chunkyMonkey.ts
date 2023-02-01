368d747373683355f7fe78aa6726954be924d853
Aswin-Shaji <aswin2000.ak@gmail.com>
2023-02-01 12:39:46 +0530

    Add 28-chunkyMonkey solution

--- 28-chunkyMonkey/chunkyMonkey.ts
+++ 28-chunkyMonkey/chunkyMonkey.ts
@@ -1,4 +1,11 @@
 export function chunkyMonkey(arr: any[], size: number): any[][] {
+  const chunkyMonkey: Array<Array<any>> = [];
+
+  for (let i = 0; i < arr.length; i+=size) {
+    chunkyMonkey.push(arr.slice(i, i+size));
+  }
+
+  return chunkyMonkey;
 }
 
 // console.log(chunkyMonkey(["a", "b", "c", "d"], 2));
