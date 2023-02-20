ab7013b4b2b52e21278ae1d0cd8265c87ea940d8
Aswin-Shaji <aswin2000.ak@gmail.com>
2023-02-20 15:49:05 +0530

    Add 31-companyBotStrategy solution

--- 31-companyBotStrategy/companyBotStrategy.ts
+++ 31-companyBotStrategy/companyBotStrategy.ts
@@ -1,4 +1,17 @@
 export function companyBotStrategy(trainingData: number[][]): number {
+  let count = 0;
+
+  const sum = trainingData.reduce((total, currentValue) => {
+    if (currentValue[1] === 1) {
+      count++;
+
+      return total += currentValue[0];
+    }
+
+    return total;
+  }, 0);
+
+  return sum / count || 0;
 }
 
 // console.log(companyBotStrategy([[3, 1], [6, 1], [4, 1], [5, 1]]));
