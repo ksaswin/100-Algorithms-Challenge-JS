0b69607f2dcd40fd044f5b0f544d63a7a1e44d6f
Aswin-Shaji <aswin2000.ak@gmail.com>
2023-02-20 16:57:35 +0530

    Add 33-composeRanges modified solution

--- 33-composeRanges/composeRanges.ts
+++ 33-composeRanges/composeRanges.ts
@@ -1,25 +1,52 @@
+// Second solution
+// The one without the currentRange variable
+
 export function composeRanges(nums: number[]): string[] {
   const numberRanges: Array<string> = [];
-  let currentRange = 0;
   
   numberRanges.push(`${nums[0]}->`);
 
   for (let i = 0; i < nums.length - 1; i++) {
     if (nums[i] + 1 !== nums[i+1]) {
-      numberRanges[currentRange] += `${nums[i]}`;
+      numberRanges[numberRanges.length - 1] += `${nums[i]}`;
 
       numberRanges.push(`${nums[i+1]}`);
-      currentRange++;
 
       if (i+2 !== nums.length) {
-        numberRanges[currentRange] += '->';
+        numberRanges[numberRanges.length - 1] += '->';
       }
     } else if (i+2 === nums.length) {
-      numberRanges[currentRange] += `${nums[i+1]}`;
+      numberRanges[numberRanges.length - 1] += `${nums[i+1]}`;
     }
   }
 
   return numberRanges;
 }
+
+// First solution
+//
+// export function composeRanges(nums: number[]): string[] {
+//   const numberRanges: Array<string> = [];
+//   let currentRange = 0;
+//   
+//   numberRanges.push(`${nums[0]}->`);
+//
+//   for (let i = 0; i < nums.length - 1; i++) {
+//     if (nums[i] + 1 !== nums[i+1]) {
+//       numberRanges[currentRange] += `${nums[i]}`;
+//
+//       numberRanges.push(`${nums[i+1]}`);
+//       currentRange++;
+//
+//       if (i+2 !== nums.length) {
+//         numberRanges[currentRange] += '->';
+//       }
+//     } else if (i+2 === nums.length) {
+//       numberRanges[currentRange] += `${nums[i+1]}`;
+//     }
+//   }
+//
+//   return numberRanges;
+// }
 
 // console.log(composeRanges([-1, 0, 1, 2, 6, 7, 9]));
