d03f94fb25418f99780cf582b6674987096a657d
Aswin-Shaji <aswin2000.ak@gmail.com>
2023-02-20 16:26:53 +0530

    Add 33-composeRanges solution

--- 33-composeRanges/composeRanges.ts
+++ 33-composeRanges/composeRanges.ts
@@ -1,4 +1,25 @@
 export function composeRanges(nums: number[]): string[] {
+  const numberRanges: Array<string> = [];
+  let currentRange = 0;
+  
+  numberRanges.push(`${nums[0]}->`);
+
+  for (let i = 0; i < nums.length - 1; i++) {
+    if (nums[i] + 1 !== nums[i+1]) {
+      numberRanges[currentRange] += `${nums[i]}`;
+
+      numberRanges.push(`${nums[i+1]}`);
+      currentRange++;
+
+      if (i+2 !== nums.length) {
+        numberRanges[currentRange] += '->';
+      }
+    } else if (i+2 === nums.length) {
+      numberRanges[currentRange] += `${nums[i+1]}`;
+    }
+  }
+
+  return numberRanges;
 }
 
 // console.log(composeRanges([-1, 0, 1, 2, 6, 7, 9]));
