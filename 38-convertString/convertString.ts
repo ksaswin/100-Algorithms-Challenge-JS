021ad63b2f33b06ee2b952aebaa4ef064e1f1e32
Aswin-Shaji <aswin2000.ak@gmail.com>
2023-04-07 17:03:17 +0530

    Add 38-convertString solution

--- 38-convertString/convertString.ts
+++ 38-convertString/convertString.ts
@@ -1,3 +1,15 @@
 export function convertString(s: string, t: string): boolean {
+  let index = 0;
+  let canConvert = true;
 
+  t.split('').forEach((tChar) => {
+    const indexInS = s.substr(index).indexOf(tChar);
+    if (indexInS === -1) {
+      canConvert = false;
+    } else {
+      index += (indexInS + 1);
+    }
+  });
+
+  return canConvert;
 }
