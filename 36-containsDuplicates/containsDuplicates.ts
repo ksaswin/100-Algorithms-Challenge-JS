a77a091a3ee94f0c0eb5ab84dbabc09bbe9d2f82
Aswin-Shaji <aswin2000.ak@gmail.com>
2023-04-07 16:39:49 +0530

    Add 36-containsDuplicates new solution

--- 36-containsDuplicates/containsDuplicates.ts
+++ 36-containsDuplicates/containsDuplicates.ts
@@ -1,3 +1,17 @@
+// Second solution
 export function containsDuplicates(a: number[]): boolean {
-  return !((new Set(a)).size === a.length);
+  a.sort((val1, val2) => val1-val2);
+
+  for (let i = 0; i < a.length; i++) {
+    if (a[i] === a[i + 1]) {
+      return true;
+    }
+  }
+
+  return false;
 }
+
+// First solution
+// export function containsDuplicates(a: number[]): boolean {
+//   return !((new Set(a)).size === a.length);
+// }
