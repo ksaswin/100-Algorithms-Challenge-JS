74131c87e21e021edb219cc6824b37c6ff85fd34
Aswin-Shaji <aswin2000.ak@gmail.com>
2023-04-07 16:20:04 +0530

    Add 35-containsCloseNums solution

--- 35-containsCloseNums/containsCloseNums.ts
+++ 35-containsCloseNums/containsCloseNums.ts
@@ -1,3 +1,28 @@
-export function containsCloseNums(nums: number[], k: number): boolean {
+export function containsCloseNums(nums: number[], target: number): boolean {
+  const numsSet = new Set(nums)
 
+  if (numsSet.size === nums.length) {
+    return false; // Since there are no repeated values
+  }
+
+  let containsCloseNums = false;
+
+  numsSet.forEach((setValue: number) => {
+    const indices = [];
+
+    nums.forEach((num: number, index: number) => {
+      if (num === setValue) {
+        indices.forEach((savedIndex) => {
+          if (Math.abs(savedIndex - index) <= target) {
+            containsCloseNums = true;
+          }
+        });
+
+        indices.push(index);
+      }
+
+    });
+  });
+
+  return containsCloseNums;
 }
