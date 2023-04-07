0ea094147dba7f438ace86c61177eba1c520a173
Aswin-Shaji <aswin2000.ak@gmail.com>
2023-04-07 16:24:33 +0530

    Add 35-containsCloseNums new solution

--- 35-containsCloseNums/containsCloseNums.ts
+++ 35-containsCloseNums/containsCloseNums.ts
@@ -1,28 +1,48 @@
+// Second solution
 export function containsCloseNums(nums: number[], target: number): boolean {
-  const numsSet = new Set(nums)
-
-  if (numsSet.size === nums.length) {
-    return false; // Since there are no repeated values
+  for (let i = 0; i < nums.length; i++) {
+    for (let j = 0; j < nums.length; j++) {
+      if (i !== j) {
+        if (nums[i] === nums[j]) {
+          if (Math.abs(i - j) <= target) {
+            return true;
+          }
+        }
+      }
+    }
   }
 
-  let containsCloseNums = false;
-
-  numsSet.forEach((setValue: number) => {
-    const indices = [];
-
-    nums.forEach((num: number, index: number) => {
-      if (num === setValue) {
-        indices.forEach((savedIndex) => {
-          if (Math.abs(savedIndex - index) <= target) {
-            containsCloseNums = true;
-          }
-        });
-
-        indices.push(index);
-      }
-
-    });
-  });
-
-  return containsCloseNums;
+  return false;
 }
+
+
+
+// First Solution
+// export function containsCloseNums(nums: number[], target: number): boolean {
+//   const numsSet = new Set(nums)
+//
+//   if (numsSet.size === nums.length) {
+//     return false; // Since there are no repeated values
+//   }
+//
+//   let containsCloseNums = false;
+//
+//   numsSet.forEach((setValue: number) => {
+//     const indices = [];
+//
+//     nums.forEach((num: number, index: number) => {
+//       if (num === setValue) {
+//         indices.forEach((savedIndex) => {
+//           if (Math.abs(savedIndex - index) <= target) {
+//             containsCloseNums = true;
+//           }
+//         });
+//
+//         indices.push(index);
+//       }
+//
+//     });
+//   });
+//
+//   return containsCloseNums;
+// }
