a60b736721342b3035a7e394c330d78bc9786d5d
Aswin-Shaji <aswin2000.ak@gmail.com>
2023-04-11 18:50:03 +0530

    Add 41-differentSymbolsNaive new solution

--- 41-differentSymbolsNaive/differentSymbolsNaive.ts
+++ 41-differentSymbolsNaive/differentSymbolsNaive.ts
@@ -1,12 +1,18 @@
+// Second solution
 export function differentSymbolsNaive(s: string): number {
-  const uniqueChars = [];
-
-  s.split('').forEach((char) => {
-    if (!uniqueChars.includes(char)) {
-      uniqueChars.push(char);
-    }
-  });
-
-  return uniqueChars.length;
+  return (new Set(s)).size;
 }
 
+// First solution
+// export function differentSymbolsNaive(s: string): number {
+//   const uniqueChars = [];
+//
+//   s.split('').forEach((char) => {
+//     if (!uniqueChars.includes(char)) {
+//       uniqueChars.push(char);
+//     }
+//   });
+//
+//   return uniqueChars.length;
+// }
+//
