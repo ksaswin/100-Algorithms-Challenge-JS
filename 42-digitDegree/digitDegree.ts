409a114df38a01e3e494df267f98e13e0059431f
Aswin-Shaji <aswin2000.ak@gmail.com>
2023-04-11 19:08:13 +0530

    Add 42-digitDegree solution

--- 42-digitDegree/digitDegree.ts
+++ 42-digitDegree/digitDegree.ts
@@ -1,3 +1,11 @@
 export function digitDegree(n: number): number {
+  let count = 0;
+
+  while(n > 9) {
+    count++;
+    n = n.toString().split('').reduce((sum, intValue) => sum + parseInt(intValue) , 0);
+  }
+
+  return count;
 }
 
