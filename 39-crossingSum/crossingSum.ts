043e3c926345a4e78b171243fd771908e8fcb06d
Aswin-Shaji <aswin2000.ak@gmail.com>
2023-04-11 18:19:03 +0530

    Add 39-crossingSum solution

--- 39-crossingSum/crossingSum.ts
+++ 39-crossingSum/crossingSum.ts
@@ -1,3 +1,12 @@
 export function crossingSum(matrix: number[][], a: number, b: number): number {
+  let total = matrix[a].reduce((sum: number, currentValue: number) => {
+    return sum + currentValue;
+  }, 0);
+
+  for (let i = 0; i < matrix.length; i++) {
+    total += i === a ?0 : matrix[i][b];
+  }
+
+  return total;
 }
 
