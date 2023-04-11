9640e9628d51c21a50fed1394976f14709dfea47
Aswin-Shaji <aswin2000.ak@gmail.com>
2023-04-11 19:20:17 +0530

    Add 43-domainType solution

--- 43-domainType/domainType.ts
+++ 43-domainType/domainType.ts
@@ -1,3 +1,22 @@
 export function domainType(domains: string[]): string[] {
+  const domainCategories = {
+    'com': 'commercial',
+    'org': 'organization',
+    'net': 'network',
+    'info': 'information',
+  };
+
+  const domainTypes = [];
+
+  domains.forEach((domain) => {
+    const splitDomain = domain.split('.');
+    const domainType = splitDomain[splitDomain.length - 1];
+    
+    if (Object.keys(domainCategories).includes(domainType)) {
+      domainTypes.push(domainCategories[domainType]);
+    }
+  });
+
+  return domainTypes;
 }
 
